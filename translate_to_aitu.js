a1ec0ea4ceba263135bd8199ae43ecf7e00bd394
Unzor <71789103+Unzor@users.noreply.github.com>
2022-02-17 14:12:15 -0600

    Update translate_to_aitu.js

--- translate_to_aitu.js
+++ translate_to_aitu.js
@@ -6,7 +6,7 @@ function translate_to_aitu(str) {
 return str.split("").map(x => String.fromCharCode(x.charCodeAt(0) + 5)).join("");
 }
 
-fs.writeFileSync(file.split(".").slice(0, -1).join(".") + ".aitu", translate_to_aitu(fs.readFileSync(file).toString()))
+fs.writeFileSync(file.split(".").slice(0, -1).join(".") + ".aspwn", translate_to_aitu(fs.readFileSync(file).toString()))
 } else {
 console.log("ERROR: no file to translate found!");
 }

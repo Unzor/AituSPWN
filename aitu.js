69359518a6b60abd6a0fd360d1f7ea6a81cdc542
Unzor <71789103+Unzor@users.noreply.github.com>
2022-02-16 22:02:48 -0600

    Update aitu.js

--- aitu.js
+++ aitu.js
@@ -3,13 +3,7 @@ const child_process = require("child_process");
 var fs = require("fs");
 var other_args = process.argv.slice(3);
 function translate_to_origin(str) {
-var r2 = [];
-var r = str.split("");
-r.forEach(function(char, i){
-var cc = char.charCodeAt(0) - 5;
-r2.push(String.fromCharCode(cc))
-})
-return r2.join("");
+return str.split("").map(x => x = String.fromCharCode(x.charCodeAt(0) - 5)).join("")
 }
 var ranum = Math.random().toString().split(".").pop();
 fs.writeFileSync("tmp_" + ranum + ".spwn", translate_to_origin(fs.readFileSync(process.argv[2]).toString()));

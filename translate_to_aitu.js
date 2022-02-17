ba00fe7850c1ab601400e88eb0ee9245b5f02fb1
Unzor <71789103+Unzor@users.noreply.github.com>
2022-02-16 22:09:43 -0600

    Update translate_to_aitu.js

--- translate_to_aitu.js
+++ translate_to_aitu.js
@@ -3,12 +3,7 @@ var file = process.argv[2];
 
 if (file) {
 function translate_to_aitu(str) {
-str = str.split("");
-var h = [];
-str.forEach(function(e){
-h.push(String.fromCharCode(e.charCodeAt(0) + 5))
-})
-return h.join("");
+return str.split("").map(x => String.fromCharCode(x.charCodeAt(0) + 5)).join("");
 }
 
 fs.writeFileSync(file.split(".").slice(0, -1).join(".") + ".aitu", translate_to_aitu(fs.readFileSync(file).toString()))
